0dfeecc1d9529fa272328cd7dbfd9e4d78d8be98
Ben Hansen <bhbenjaminhansen@gmail.com>
2020-12-14 20:31:49 -0700

    fixed wasm code

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,16 @@ import rust from "@wasm-tool/rollup-plugin-rust";
 
 export default {
     input: {
-        foo: "Cargo.toml",
+        example: "Cargo.toml",
+    },
+    output: {
+        dir: "dist/js",
+        format: "iife",
+        sourcemap: true,
     },
     plugins: [
-        rust(),
+        rust({
+            serverPath: "js/"
+        }),
     ],
 };
--- src/lib.rs
+++ src/lib.rs
@@ -132,8 +132,9 @@ fn rand_char() -> char {
 }
 
 use wasm_bindgen::prelude::*;
+use web_sys::console;
 #[wasm_bindgen(start)]
 pub fn main_js() {
     console_error_panic_hook::set_once();
-    println!("Please work!");
+    console::log_1(&JsValue::from("Hello, world!"));
 }
